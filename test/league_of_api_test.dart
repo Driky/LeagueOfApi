2e87b345bdae11c49fad6babb818080e60bff584
Cedric Chatelain <cedric.chatelain@protonmail.com>
2021-03-16 23:48:49 -0400

    Change dep version and null safety test fix

--- test/league_of_api_test.dart
+++ test/league_of_api_test.dart
@@ -1,15 +1,23 @@
+import 'package:dio/dio.dart';
 import 'package:league_of_api/league_of_api.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('Init client', () {
-    LeagueOfApi client;
+    LeagueOfApi? client;
 
     setUp(() {
       client = LeagueOfApi(log: true);
     });
 
     test('Client is initialized', () {
+      expect(client, isA<LeagueOfApi>());
+    });
+
+    test('Client is initialized with given dio instance', () {
+      var dio = Dio();
+      client = LeagueOfApi(client: dio);
+
       expect(client, isA<LeagueOfApi>());
     });
   });
