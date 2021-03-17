38260a6b80b12770b4790aa7ea3119f0e10a66da
Cedric Chatelain <cedric.chatelain@protonmail.com>
2021-03-17 09:48:05 -0400

    Null safety stuff

--- lib/src/league_of_api_base.dart
+++ lib/src/league_of_api_base.dart
@@ -2,12 +2,12 @@ import 'package:dio/dio.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class LeagueOfApi {
-  Dio _client;
+  late Dio _client;
   static const int _CONNECT_TIMEOUT = 5000;
   static const int _RECEIVE_TIMEOUT = 3000;
   static const int _SEND_TIMEOUT = 3000;
 
-  LeagueOfApi({Dio client, bool log = false}) {
+  LeagueOfApi({Dio? client, bool log = false}) {
     if (client != null) {
       _client = client;
     } else {
