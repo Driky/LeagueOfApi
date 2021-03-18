9456d7bc9ac01b2063dad5b5d0291161f2cc4760
Cedric Chatelain <cedric.chatelain@protonmail.com>
2021-03-18 06:00:30 -0400

    Add new test to barrel file

--- test/test_all.dart
+++ test/test_all.dart
@@ -1,7 +1,9 @@
 import 'package:test/test.dart';
 
 import 'league_of_api_test.dart' as league_of_api;
+import 'tag_line_test.dart' as tag_line;
 
 void main() {
   group('league_of_api', league_of_api.main);
+  group('tag_line', tag_line.main);
 }
